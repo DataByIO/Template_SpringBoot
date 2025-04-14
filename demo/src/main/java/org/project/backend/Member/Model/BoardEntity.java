f1f97fcf7c6ab1f0952937cd5a61d2508054eb0e
DataByIO <ro_bot__@naver.com>
2025-04-14 11:54:54 +0900

    KIMDONGMIN:

--- demo/src/main/java/org/project/backend/Member/Model/BoardEntity.java
+++ demo/src/main/java/org/project/backend/Member/Model/BoardEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class BoardEntity {
     @Id
     @GeneratedValue
-    private Long id;
+    private String id;
     private String title;
     private String content;
     private String author;
