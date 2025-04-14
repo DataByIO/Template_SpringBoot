2e495274923ec06ecc584e84612b9e98f5c480d4
DataByIO <ro_bot__@naver.com>
2025-04-14 15:09:02 +0900

    KIMDONGMIN:

---
+++ demo/src/main/java/org/project/backend/Member/Controller/BoardController.java
@@ -0,0 +1,52 @@
+package org.project.backend.Member.Controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.project.backend.SecurityService.Model.MemberEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+
+@Controller
+@ResponseBody
+public class BoardController {
+
+
+    @Autowired
+    //private BoardService boardService;
+
+    @PostMapping("/boardSelect")
+    //@RequestBody
+    public ResponseEntity boardSelect(HttpServletResponse res) throws Exception {
+        //HashMap<String, Object> boardService = boardSelect();
+
+        return new ResponseEntity<>(HttpStatus.OK);// Password 암호화 체크
+    }
+
+    @PostMapping("/boardInsert")
+    //@RequestBody
+    public ResponseEntity boardInsert(MemberEntity memberEntity, HttpServletResponse res) throws Exception {
+        //HashMap<String, Object> boardService = boardSelect();
+        return new ResponseEntity<>(HttpStatus.OK);// Password 암호화 체크
+    }
+
+    @PostMapping("/boardDelete")
+    //@RequestBody
+    public ResponseEntity boardDelete(MemberEntity memberEntity, HttpServletResponse res) throws Exception {
+        //HashMap<String, Object> boardService = boardSelect();
+        return new ResponseEntity<>(HttpStatus.OK);// Password 암호화 체크
+    }
+
+    @PostMapping("/boardUpdate")
+    //@RequestBody
+    public ResponseEntity boardUpdate(MemberEntity memberEntity, HttpServletResponse res) throws Exception {
+        //HashMap<String, Object> boardService = boardSelect();
+        return new ResponseEntity<>(HttpStatus.OK);// Password 암호화 체크
+    }
+
+
+}
--- demo/src/main/java/org/project/backend/Member/Model/BoardEntity.java
+++ demo/src/main/java/org/project/backend/Member/Model/BoardEntity.java
@@ -14,24 +14,22 @@ import java.time.LocalDateTime;
 public class BoardEntity {
     @Id
     @GeneratedValue
-    private String id;
-    private String title;
-    private String content;
-    private String author;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private String comment;
+    private int boardNumber;//게시글번호 PK
+    private String id;//아이디
+    private String title;//제목
+    private String content;//내용
+    private String author;//작성자
+    private LocalDateTime createdAt;//작성시
+    private LocalDateTime updatedAt;//업데이트
+    private String comment;//댓글 FK
     private String commentYn;//댓글 허용여부
-    private String notesYn;
-    private String secret;
-    private String image;
-    private String ipAddress;
-    private String status;
-    private String reportedCount;
-    private String attachment;
-
-
-    private int views;
-    // 게스트 글 비밀번호용 컬럼 (필요한 경우)
-    private String password;
+    private String notesYn;//공지여부
+    private String secret;// 비밀글
+    private String image;//이미지
+    private String ipAddress;//작성자아이피
+    private String status;//게시글 상태
+    private String reportedCount;//게시글 신고 건 수
+    private String attachment;//첨부파일
+    private int views;//조회수
+    private String password;// 게스트 글 비밀번호용 컬럼 (필요한 경우)
 }
